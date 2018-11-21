6de8e573d5c4514ba2ddae06aab9fc64fd004d55
autoseleniumtesting@gmail.com <autoseleniumtesting@gmail.com>
2018-11-21 22:45:12 +0530

    s
    project

---
+++ src/main/java/projectfor/projectfor/App.java
@@ -0,0 +1,13 @@
+package projectfor.projectfor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/projectfor/projectfor/AppTest.java
@@ -0,0 +1,14 @@
+package projectfor.projectfor;
+
+import org.junit.Test;
+
+
+public class AppTest
+{
+
+	@Test
+	public void testApp()
+    {
+        System.out.println("Sumit");
+    }
+}
